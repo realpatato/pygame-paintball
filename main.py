de4fc95a70d4b21ad8012b0fd1d941af78c31a42
realpatato <you@example.com>
2025-06-05 10:53:42 -0400

    splatters are generated and drawn to screen. they have random colors and random shapes.

---
+++ main.py
@@ -0,0 +1,81 @@
+''' IMPORTS '''
+#needed for all visual aspects of the program
+import pygame
+#needed for color selection
+import random
+#needed for splat creation and drawing
+import splat
+
+#starts the module
+pygame.init()
+
+''' WINDOW SET UP '''
+#variables for screen size for easy changing
+screen_width = 1000
+screen_height = 750
+
+#creates a screen with given dimensions
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#set a caption for the window
+pygame.display.set_caption("Paintball")
+
+''' VARIABLE SET UP'''
+#color code constants, for colors that will never change
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PINK = (255, 0, 255)
+
+#list to store the colors, for random selection
+colors = [WHITE, RED, GREEN, BLUE, YELLOW, CYAN, PINK]
+
+#empty list to store splats
+splats = []
+
+''' GAME LOOP SET UP '''
+#clock to manage the frame rate
+clock = pygame.time.Clock()
+
+#variable to control if the game loop is running or not
+keep_playing = True
+
+''' GAME LOOP '''
+#the loop itself, checks if the loop is supposed to be going
+while keep_playing:
+    #checking for any events (quitting, clicking, key press)
+    for event in pygame.event.get():
+        #check for mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #get the mouse position
+            mouse_pos = pygame.mouse.get_pos()
+            #create and add a Splat object to the splat list
+            splats.append(splat.Splat(colors[random.randint(0, len(colors) - 1)], mouse_pos))
+
+        #check if the player has quit
+        if event.type == pygame.QUIT:
+            #set control variable to false
+            keep_playing = False
+    
+    #clear screen before anything else
+    screen.fill(BLACK)
+
+    ''' DRAW ALL ITEMS HERE '''
+    for splat_object in splats:
+        splat_object.draw_splat(screen)
+
+    #updates the screen
+    pygame.display.update()
+
+    #sets the frame rate
+    clock.tick(60)
+
+''' ENDING THE PROGRAM '''
+#quits the window
+pygame.quit()
+#ends the program
+quit()
---
+++ splat.py
@@ -0,0 +1,29 @@
+''' CREATES THE SPLAT CLASS '''
+''' IMPORTS '''
+#needed in order to draw the splat
+import pygame
+#needed in order to calaculate points of the circle
+import math
+#needed in order to create the noise, creating a unique shape
+import random
+
+''' SPLAT CLASS '''
+class Splat():
+    def __init__(self, color, center):
+        self._color = color
+        self._center = center
+        self._splat_points = self.get_splat_points()
+    
+    def get_splat_points(self):
+        points = []
+        angle = 0
+        while angle < math.pi*2:
+            radius = 5 + random.random() * 20
+            x_pos = radius * math.cos(angle) + self._center[0]
+            y_pos = radius * math.sin(angle) + self._center[1]
+            points.append((x_pos, y_pos))
+            angle+=0.1
+        return points
+
+    def draw_splat(self, surface):
+        pygame.draw.polygon(surface, self._color, self._splat_points)
